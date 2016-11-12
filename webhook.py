4a5226ac1d74a80ab09f70002cee51edffde9a6b
weet <thewawar@gmail.com>
2016-11-12 20:34:27 +0800

    Fix webhook error

--- webhook.py
+++ webhook.py
@@ -1,21 +1,30 @@
 #!/usr/bin/env python
-#coding: utf-8
+# coding: utf-8
 
 import os
 import json
+import hmac
+import hashlib
 
-from flask import Flask, request
+from flask import Flask, request, current_app
 
 app = Flask(__name__)
 
 
+def verify_sign(hub_secret, sign, payload):
+    sign_our = hmac.new(hub_secret, payload, hashlib.sha1).hexdigest()
+    return sign == 'sha1={}'.format(sign_our)
+
+
 @app.route('/webhook', methods=['POST'])
 def webhook():
-    data = json.loads(request.data)
-    if data['actor']['username'].lower() == 'thewawar':
-        os.system('git pull')
-        os.system('jekyll build')
-        return 'ok'
+    sign = request.headers.get('X-Hub-Signature')
+    if not verify_sign(current_app.config['HUB_SECRET'], sign, request.data):
+        return 'Invalid Signature', 403
+
+    os.system('git pull')
+    os.system('jekyll build')
+    return 'ok'
 
 
 if __name__ == '__main__':
